5f9a384205e56ef0bbb688b389ae9e5a21acaa4d
NGUYEN KHAC DIEP <khacdieptkv@gmail.com>
2012-10-10 19:31:30 +0700

    Add Module API
    Add Module API

---
+++ mantech_mod_api/src/mantech/mod/admin/article/api/AdminArticleBiz.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mantech.mod.admin.article.api;
+
+import java.util.List;
+import mantech.mod.article.entity.Article;
+
+/**
+ *
+ * @author NDC
+ */
+public interface AdminArticleBiz {
+
+    void create(Article article);
+
+    void edit(Article article);
+
+    void remove(Article article);
+
+    Article find(Object id);
+
+    List<Article> findAll();
+}
---
+++ mantech_mod_api/src/mantech/mod/admin/article/api/AdminFQABiz.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mantech.mod.admin.article.api;
+
+import java.util.List;
+import mantech.mod.article.entity.Faq;
+
+/**
+ *
+ * @author NDC
+ */
+public interface AdminFQABiz {
+
+    void create(Faq faq);
+    void edit(Faq faq);
+    void remove(Faq faq);
+    Faq find(Object id);
+    List<Faq> findAll();
+    
+}
