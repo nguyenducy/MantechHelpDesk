4a363ec70ac77b9002187aa0fbd00789ad10d9e4
Duc Y <nguyenducy@gmail.com>
2012-10-11 20:22:55 +0700

    Update package' name on api module

---
+++ mantech_mod_api/src/mantech/mod/account/api/AdminProfileBiz.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mantech.mod.account.api;
+
+import java.util.List;
+import mantech.mod.account.entity.Profile;
+
+/**
+ *
+ * @author NDC
+ */
+public interface AdminProfileBiz {
+
+    void create(Profile profile);
+
+    void edit(Profile profile);
+
+    void remove(Profile profile);
+
+    Profile find(Object id);
+
+    List<Profile> findAll();
+
+    Profile searchDeparment(String deparmentId);
+}
---
+++ mantech_mod_api/src/mantech/mod/account/api/AdminRoleBiz.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mantech.mod.account.api;
+
+import java.util.List;
+import mantech.mod.account.entity.Role;
+
+/**
+ *
+ * @author NDC
+ */
+public interface AdminRoleBiz {
+
+    void create(Role role);
+
+    void edit(Role role);
+
+    void remove(Role role);
+
+    Role find(Object id);
+
+    List<Role> findAll();
+}
